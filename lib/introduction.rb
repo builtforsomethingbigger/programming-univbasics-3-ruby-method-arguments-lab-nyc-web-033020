54ddee52dd75963dfceba46702909f36e67f2a14
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-18 23:47:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,8 +4,10 @@ end
 introduction("Steve")
 
 
-
-
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I'm learning #{language}."
+end
+introduction_with_language("Steve", "Ruby")
 
 
 
